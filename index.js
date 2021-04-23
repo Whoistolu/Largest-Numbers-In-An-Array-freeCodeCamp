c9833a675d0efacdffa1b54c9073ba4e5e9821de
Ajse <digittolu25@gmail.com>
2021-04-23 23:44:15 +0100

    add alternativve solution

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-function largestOfFour(arr) {
-    let maxes = [];
+// function largestOfFour(arr) {
+//     let maxes = [];
+//     for (var i= 0; i < arr.length; i++) {
+//         let tempMax = arr[i][0];
+//         for (var j= 0; j < arr.length j++) {
+//             let currentElement = arr[i][j];
+//             if (currentElement >= tempMax) {
+
+//             }
+//         }
+//         maxes.push(tempMax)
+//     }
+//     return arr;
+//   }
+  
+//   console.log(largestOfFour[[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+  function largestOfFour(arr) {
+    let maxes = [0, 0, 0, 0];
     for (var i= 0; i < arr.length; i++) {
-        let tempMax = arr[i][0];
         for (var j= 0; j < arr.length j++) {
             let currentElement = arr[i][j];
-            if (currentElement >= tempMax) {
-
+            if (currentElement >= maxes[i]) {
+                maxes[i] = currentElement;
             }
         }
-        maxes.push(tempMax)
     }
     return arr;
   }
